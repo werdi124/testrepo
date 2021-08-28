6fb3f50e0485f5bf9bdff55f691ad5cc39cd36e3
werdi124 <werdigunawan@gmail.com>
2021-08-28 17:53:11 +0900

    Add files via upload

---
+++ tes1.py
@@ -0,0 +1,7 @@
+squares = ['orange', 'orange', 'purple', 'blue ', 'orange']
+new_squares = []
+i = 0
+while(i < len(squares) and squares[i] == 'orange'):
+    new_squares.append(squares[i])
+    i = i + 1
+print (new_squares)
